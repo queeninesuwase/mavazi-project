779bbff39e019a0f953d4a56f161432387c4bd1d
John Owuor <owuor91@gmail.com>
2026-07-13 19:06:48 +0300

    add login screen

---
+++ android/app/src/main/kotlin/com/example/mavazi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mavazi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() {
+    return _LoginScreenState();
+  }
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _loginFormKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _hidePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24),
+          child: Form(
+            key: _loginFormKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  "assets/images/logo.png",
+                  width: 160,
+                  height: 160,
+                  fit: BoxFit.contain,
+                ),
+
+                SizedBox(height: 16),
+
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 16),
+
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter email';
+                    }
+
+                    if (!value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 24),
+
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outline),
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _hidePassword = !_hidePassword;
+                        });
+                      },
+                      icon: Icon(
+                        _hidePassword
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                      ),
+                    ),
+                  ),
+                  obscureText: _hidePassword,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your password';
+                    }
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 24),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login", style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 16),
+
+                Row(children: [
+                  Text("Don't have an account?"),
+                  TextButton(onPressed: (){}, child: Text("Sign Up"))
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
